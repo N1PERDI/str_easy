b2ff5dba5536fe3e6ee76c12663e70e0f8675711
N1PERDI <143997425+N1PERDI@users.noreply.github.com>
2023-10-01 22:08:17 +0300

    Update str_easy.h

--- str_easy.h
+++ str_easy.h
@@ -1,3 +1,4 @@
+
 #include <string>
 #include <iostream>
 using namespace std;
@@ -9,3 +10,6 @@ void itc_first_end_three(string str);
 int itc_count_char_in_str(char ch, string str);
 string itc_even_place(string str);
 double itc_percent_lower_uppercase(string str);
+string itc_reverse_str(string str);
+string itc_slice_str(string str, int start, int end) ;
+bool itc_equal_reverse(string str);
