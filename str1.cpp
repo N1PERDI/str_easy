c00d54ee9c576151d7135657f5f6f21370950ffb
N1PERDI <143997425+N1PERDI@users.noreply.github.com>
2023-10-01 22:07:29 +0300

    Update str1.cpp

--- str1.cpp
+++ str1.cpp
@@ -9,16 +9,16 @@ long long itc_len(string str) {
 }
 void itc_print_copy_str(string str, int number) {
 	int s = 0;
-	while (s<number)
+	while (s < number)
 	{
 		cout << str;
 		s++;
-	}	
+	}
 }
 void itc_first_end_three(string str) {
 	if (size(str) > 5)
 	{
-		cout << str[0] << str[1] << str[2]<< str[size(str)-3] << str[size(str) - 2] << str[size(str) - 1];
+		cout << str[0] << str[1] << str[2] << str[size(str) - 3] << str[size(str) - 2] << str[size(str) - 1];
 	}
 	else {
 		int s = size(str);
@@ -27,16 +27,16 @@ void itc_first_end_three(string str) {
 			cout << str[0];
 			s--;
 		}
-		
+
 	}
 }
 int itc_count_char_in_str(char ch, string str) {
 	int k = 0;
 	int size = itc_len(str);
-	for (int n = 0; n < size; n++) 
+	for (int n = 0; n < size; n++)
 	{
 		if (ch == str[n])
-			 k++;
+			k++;
 	}
 	return k++;
 }
@@ -51,24 +51,65 @@ string itc_even_place(string str) {
 			s = s + str[n];
 		}
 	}
-	if (s == "" ) { return "-1"; }
-	else 
-	return s;
+	if (s == "") { return "-1"; }
+	else
+		return s;
 }
 double itc_percent_lower_uppercase(string str) {
 	double soot = 0.0;
 	double mal = 0;
 	double bol = 0;
 	for (int n = 0; n < itc_len(str); n++)
-		char ch = str[n];
-		if (ch >= 'a' &&  ch <= 'z')
-		{
-			mal++;
-		}
-		else if (ch >= 'A' && ch <= 'Z')
-		{
-			bol++;
-		}
+		if (str[n] >= 'a' && str[n] <= 'z')
+	{
+		mal++;
+	}
+	else if (str[n] >= 'A' && str[n] <= 'Z')
+	{
+		bol++;
+	}
 	soot = mal / bol;
 	return soot;
 }
+string itc_reverse_str(string str) {
+	string revstr;
+	for (int n = 0; n <= size(str); n++)
+	{
+		revstr = revstr + str[size(str) - n];
+
+	}
+	return revstr;
+}
+string itc_slice_str(string str, int start, int end) {
+	string itog;
+
+	if (end < start) {
+		itog = str;
+	}
+	else 
+	{
+		int len = itc_len(str);
+		if (end > len) 
+		{ 
+			end = len; 
+		}
+
+		for (int i = start; i <= end; i++) 
+		{
+			itog += str[i];
+		}
+	}
+
+	return itog;
+
+}
+bool itc_equal_reverse(string str) {
+		int len = str.length();
+		for (int n = 0; n < len / 2; n++) {
+			if (str[n] != str[len - n - 1])		
+			{
+				return 0;
+			}
+		}
+		return 1;
+	}
